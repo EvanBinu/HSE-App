9967dbc3a8897d43ff6f55f9108570fdc6e127b1
Evan Binu <evanbinu17@gmail.com>
2025-05-19 09:53:15 +0530

    Submit report

--- lib/pages/report_submission_page.dart
+++ lib/pages/report_submission_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../services/firestore_service.dart';
+import '../models/hse_report.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../constants.dart';
 
 class ReportSubmissionPage extends StatefulWidget {
   const ReportSubmissionPage({super.key});
@@ -8,8 +12,60 @@ class ReportSubmissionPage extends StatefulWidget {
 }
 
 class _ReportSubmissionPageState extends State<ReportSubmissionPage> {
+  final _firestoreService = FirestoreService();
+  String employeesText = '';
+  String permitsText = '';
+  String issuesText = '';
+
+  void submitReport() {
+    final report = HSEReport(
+      userId: FirebaseAuth.instance.currentUser!.uid,
+      employeesPresent: int.tryParse(employeesText) ?? 0,
+      permitsIssued: int.tryParse(permitsText) ?? 0,
+      issuesOccurred: int.tryParse(issuesText) ?? 0,
+      date: DateTime.now(),
+    );
+    _firestoreService.submitReport(report);
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Submit HSE Report')),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: [
+            RoundedTextField(
+              hintText: 'Employees Present',
+              onChanged: (value) {
+                employeesText = value;
+              },
+            ),
+            SizedBox(height: 8.0),
+            RoundedTextField(
+              hintText: 'Permits Issued',
+              onChanged: (value) {
+                permitsText = value;
+              },
+            ),
+            SizedBox(height: 8.0),
+            RoundedTextField(
+              hintText: 'Issues Occurred',
+              onChanged: (value) {
+                issuesText = value;
+              },
+            ),
+            SizedBox(height: 24.0),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Submit',
+              onPressed: submitReport,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/hse_report.dart';
+
+class FirestoreService {
+  final _db = FirebaseFirestore.instance;
+
+  Future<void> submitReport(HSEReport report) async {
+    await _db.collection('hse_reports').add(report.toMap());
+  }
+}
